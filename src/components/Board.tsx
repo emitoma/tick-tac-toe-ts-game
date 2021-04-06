a1d8132915a3f29289f12c069bab27e0e096c475
emitoma <toma.emi.95.07.17@gmail.com>
2021-04-06 15:20:54 +0200

    set up project, create basic components for board

---
+++ src/components/Board.tsx
@@ -0,0 +1,10 @@
+import Square from "./Square";
+
+const Board = () => {
+    return <div>
+        <Square/>
+        <Square/>
+        <Square/>
+    </div>
+}
+export default Board;
---
+++ src/components/Square.tsx
@@ -0,0 +1,6 @@
+import css from "../styles/square.module.scss"
+
+const Square = () => {
+    return <div className={css["square"]}></div>
+}
+export default Square;
